1c3b442e26354c2a1181b0e248ba32a6a3cfd575
Alex Ng <alexkng97@gmail.com>
2021-01-04 15:50:29 +0000

    createTrainees method implemented and tested

--- src/main/java/com/sparta/spartaSimulator/controller/TraineeManager.java
+++ src/main/java/com/sparta/spartaSimulator/controller/TraineeManager.java
@@ -1,4 +1,21 @@
 package com.sparta.spartaSimulator.controller;
 
+import com.sparta.spartaSimulator.model.Trainee;
+
+import java.util.ArrayList;
+
 public class TraineeManager {
+
+    public static Trainee[] createTrainees(int randomNumber){
+        //done as array as size is passed through, but can be changed to what is used in other classes
+
+        Trainee[] result = new Trainee[randomNumber];
+        for(int i = 0; i < randomNumber; i++){
+            result[i] = new Trainee();
+        }
+
+        return result;
+    }
+
+
 }
--- src/test/java/com/sparta/spartaSimulator/AppTest.java
+++ src/test/java/com/sparta/spartaSimulator/AppTest.java
@@ -1,12 +1,23 @@
 package com.sparta.spartaSimulator;
 
 
+import com.sparta.spartaSimulator.controller.TraineeManager;
+import com.sparta.spartaSimulator.model.Trainee;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 class AppTest {
 
     @org.junit.jupiter.api.Test
     public void shouldAnswerWithTrue() {
         Assertions.assertEquals(1, 1);
+    }
+
+    @Test
+    public void creationOfTrainees(){
+        Trainee[] test = TraineeManager.createTrainees(10);
+        Assertions.assertEquals(10, test.length);
     }
 }
