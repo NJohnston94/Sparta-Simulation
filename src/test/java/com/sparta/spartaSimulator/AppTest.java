89a9e89e28e081f72bf8d624db0abc8c22183722
MrFletcha <jwefletcher@hotmail.co.uk>
2021-01-04 16:03:27 +0000

    Alternate Test class made for FactoryTests.

--- src/test/java/com/sparta/spartaSimulator/AppTest.java
+++ src/test/java/com/sparta/spartaSimulator/AppTest.java
@@ -1,6 +1,5 @@
 package com.sparta.spartaSimulator;
 
-import com.sparta.spartaSimulator.controller.Factory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,10 +10,4 @@ class AppTest {
         Assertions.assertEquals(1, 1);
     }
 
-    @Test
-    public void factoryReturnObj()
-    {
-        Assertions.assertNotNull(Factory.factory(1));
-        Assertions.assertNotNull(Factory.factory(2));
-    }
 }
