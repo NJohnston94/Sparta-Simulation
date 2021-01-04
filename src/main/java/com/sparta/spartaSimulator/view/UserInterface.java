6bcb55ec1e0696eefdba381613b6cf53a514925b
RashawnKH <rashawn1999@hotmail.com>
2021-01-04 16:19:09 +0000

    Base added for UserInterface

--- src/main/java/com/sparta/spartaSimulator/view/UserInterface.java
+++ src/main/java/com/sparta/spartaSimulator/view/UserInterface.java
@@ -1,4 +1,34 @@
 package com.sparta.spartaSimulator.view;
 
 public class UserInterface {
+
+    public static int getUserInput(){
+        int userInput = 0;
+
+        return userInput;
+    }
+
+
+
+
+
+    /**
+     * Test via reading back in the input, if there is additional time
+     */
+
+    public static void displayResults(){
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
---
+++ src/test/java/com/sparta/spartaSimulator/WaitingListTest.java
@@ -0,0 +1,2 @@
+package com.sparta.spartaSimulator;public class WaitingListTest {
+}
