3c32c2f1b2d345d3e3968742ec78a8390e7bd75a
Ahmed AbdulRahman <a.rahman1198@gmail.com>
2021-01-04 14:36:55 +0000

    classes for project

---
+++ src/main/java/com/sparta/spartaSimulator/controller/TraineeManager.java
@@ -0,0 +1,4 @@
+package com.sparta.spartaSimulator.controller;
+
+public class TraineeManager {
+}
--- src/test/java/com/sparta/spartaSimulator/AppTest.java
+++ src/test/java/com/sparta/spartaSimulator/AppTest.java
@@ -3,10 +3,10 @@ package com.sparta.spartaSimulator;
 
 import org.junit.jupiter.api.Assertions;
 
-public class Test {
+class Test {
 
     @org.junit.jupiter.api.Test
     public void shouldAnswerWithTrue() {
-        Assertions.assertEquals(1,1);
+        Assertions.assertEquals(1, 1);
     }
 }
