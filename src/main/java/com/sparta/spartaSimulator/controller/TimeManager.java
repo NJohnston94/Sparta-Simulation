e9ef9d22eba6c6603925b10e929a6bd0dd3ef262
Daniel Alldritt <daniel.p.alldritt@bath.edu>
2021-01-04 15:54:57 +0000

    Base outline of methods needed in TimeManager and tests for those methods

--- src/main/java/com/sparta/spartaSimulator/controller/TimeManager.java
+++ src/main/java/com/sparta/spartaSimulator/controller/TimeManager.java
@@ -1,4 +1,19 @@
 package com.sparta.spartaSimulator.controller;
 
 public class TimeManager {
+
+    private static int currentTime;
+
+    public static int getCurrentTime() {
+        return currentTime;
+    }
+
+    public static void setCurrentTime(int currentTime) {
+        TimeManager.currentTime = currentTime;
+    }
+
+    public static void delayTime (int separation, int iteration) {
+
+    }
+
 }
--- src/test/java/com/sparta/spartaSimulator/AppTest.java
+++ src/test/java/com/sparta/spartaSimulator/AppTest.java
@@ -1,7 +1,9 @@
 package com.sparta.spartaSimulator;
 
 
+import com.sparta.spartaSimulator.controller.TimeManager;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class AppTest {
 
@@ -9,4 +11,17 @@ class AppTest {
     public void shouldAnswerWithTrue() {
         Assertions.assertEquals(1, 1);
     }
+
+    @Test
+    public void getSetCurrentTimeTest() {
+        TimeManager.setCurrentTime(3);
+        Assertions.assertEquals(3, TimeManager.getCurrentTime());
+    }
+
+    @Test
+    public void delayTimeTest() {
+        TimeManager.delayTime(2, 4);
+        Assertions.assertEquals(8, TimeManager.getCurrentTime());
+    }
+
 }
