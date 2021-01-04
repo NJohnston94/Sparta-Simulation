99daa3f0e663ac1352d75c70370385e5ebbc579e
RashawnKH <rashawn1999@hotmail.com>
2021-01-04 16:23:01 +0000

    Added test for UserInterface.java

--- src/test/java/com/sparta/spartaSimulator/UserInterfaceTest.java
+++ src/test/java/com/sparta/spartaSimulator/UserInterfaceTest.java
@@ -1,2 +1,17 @@
-package com.sparta.spartaSimulator;public class UserInterfaceTest {
+package com.sparta.spartaSimulator;
+
+import com.sparta.spartaSimulator.view.UserInterface;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserInterfaceTest {
+
+    @Test
+    public void checkUserInputIsCorrect(){
+
+        Assertions.assertEquals(50,UserInterface.getUserInput());
+
+    }
+
+    
 }
