def7c5480e6815acf23e954023c5757294343363
MrFletcha <jwefletcher@hotmail.co.uk>
2021-01-04 15:54:51 +0000

    Factory Added, Interface made to link factory to both Trainee and Centre, Interface implemented on Trainee class and TraineeCentre class

--- src/main/java/com/sparta/spartaSimulator/model/Trainee.java
+++ src/main/java/com/sparta/spartaSimulator/model/Trainee.java
@@ -1,6 +1,8 @@
 package com.sparta.spartaSimulator.model;
 
-public class Trainee {
+import com.sparta.spartaSimulator.controller.CreationInt;
+
+public class Trainee implements CreationInt {
 
     public enum TraineeStatus{
         PLACED,
--- src/test/java/com/sparta/spartaSimulator/AppTest.java
+++ src/test/java/com/sparta/spartaSimulator/AppTest.java
@@ -1,12 +1,20 @@
 package com.sparta.spartaSimulator;
 
-
+import com.sparta.spartaSimulator.controller.Factory;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class AppTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void shouldAnswerWithTrue() {
         Assertions.assertEquals(1, 1);
+    }
+
+    @Test
+    public void factoryReturnObj()
+    {
+        Assertions.assertNotNull(Factory.factory(1));
+        Assertions.assertNotNull(Factory.factory(2));
     }
 }
