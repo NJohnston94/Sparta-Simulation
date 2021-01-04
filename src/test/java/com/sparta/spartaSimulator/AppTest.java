3141ab2d8abac36f9020f20847bf65344c7cb5d2
Daniel Alldritt <daniel.p.alldritt@bath.edu>
2021-01-04 16:03:32 +0000

    Altered the tests to be performed in a separate class

--- src/test/java/com/sparta/spartaSimulator/AppTest.java
+++ src/test/java/com/sparta/spartaSimulator/AppTest.java
@@ -12,16 +12,4 @@ class AppTest {
         Assertions.assertEquals(1, 1);
     }
 
-    @Test
-    public void getSetCurrentTimeTest() {
-        TimeManager.setCurrentTime(3);
-        Assertions.assertEquals(3, TimeManager.getCurrentTime());
-    }
-
-    @Test
-    public void delayTimeTest() {
-        TimeManager.delayTime(2, 4);
-        Assertions.assertEquals(8, TimeManager.getCurrentTime());
-    }
-
 }
