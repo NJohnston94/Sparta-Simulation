da8b0096fcc9366a81bc43b95fb2c16cd82abcfd
GSpratt97 <greg.spratt@icloud.com>
2021-01-05 12:23:11 +0000

    User interface updated and Centre manager added counters

--- src/main/java/com/sparta/spartaSimulator/controller/CentreManager.java
+++ src/main/java/com/sparta/spartaSimulator/controller/CentreManager.java
@@ -2,15 +2,30 @@ package com.sparta.spartaSimulator.controller;
 
 import com.sparta.spartaSimulator.model.TraineeCentre;
 
+import java.util.HashMap;
+
 public class CentreManager {
+    public static HashMap<Integer, TraineeCentre> openCentres;
+    public static int numberOfFullCentres = 0;
+    public static int totalNumberOfTrainees = 0;
+
     // Capacity
     // Getters &
     // Parameters
     // Calls factory to have centre object creates
     // Maintain how many centre objects
-    // Create centres
-    public static TraineeCentre createCentre() {
-        TraineeCentre traineeCentre = new TraineeCentre();
-        return traineeCentre;
+    public static CreationInt creatingFactory() {
+        return Factory.factory(2);
     }
+
+    public static boolean isFull() {
+        for(TraineeCentre centre: openCentres.values()) {
+            if(centre.getCentreStatus() == TraineeCentre.CentreStatus.FULL) {
+                numberOfFullCentres++;
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- src/main/java/com/sparta/spartaSimulator/view/UserInterface.java
+++ src/main/java/com/sparta/spartaSimulator/view/UserInterface.java
@@ -1,5 +1,9 @@
 package com.sparta.spartaSimulator.view;
 
+import com.sparta.spartaSimulator.controller.CentreManager;
+import com.sparta.spartaSimulator.model.TraineeCentre;
+import com.sparta.spartaSimulator.model.WaitingList;
+
 public class UserInterface {
 
     public static int getUserInput(){
@@ -17,7 +21,21 @@ public class UserInterface {
      */
 
     public static void displayResults(){
+//        number of open centres
+        // Get number of centres in HashMap
+        System.out.println("Number of open centres: "+CentreManager.openCentres.size());
 
+//        number of full centres
+        // Get number of FULL centres in HashMap
+        System.out.println("Number of full centres: "+CentreManager.numberOfFullCentres);
+
+//        number of trainees currently training
+        // Loop through all centres in HashMap, counting each
+        System.out.println("Number of trainees currently training: "+CentreManager.totalNumberOfTrainees);
+
+//        number of trainees on the waiting list
+        // Waiting list size
+        System.out.println("Number of trainees on the waiting list: "+WaitingList.waitingList.size());
 
     }
 
