1f7bf2355b05ba920fdf5a0b54159412754de7bf
Alex Ng <alexkng97@gmail.com>
2021-01-05 12:02:16 +0000

    input methods implemented, duration input has input checks

--- src/main/java/com/sparta/spartaSimulator/view/UserInterface.java
+++ src/main/java/com/sparta/spartaSimulator/view/UserInterface.java
@@ -1,26 +1,59 @@
 package com.sparta.spartaSimulator.view;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class UserInterface {
 
-    public static int getUserInput(){
-        int userInput = 0;
+    public static int getDurationInput(){
+        System.out.println("Enter duration of simulation in months:");
+        int months = 0;
+        boolean validInput =false;
 
-        return userInput;
+        while(!validInput){
+            Scanner scanner = new Scanner(System.in);
+            try{
+                months = scanner.nextInt();
+                //if wanting an upper limit: edit statement below to an OR statement with upper bound
+                while(months < 1){
+                    System.out.println("Enter valid number!");
+                    months =scanner.nextInt();
+                }
+                validInput = true;
+
+            }catch(InputMismatchException e){
+                System.out.println("Enter a number!");
+                validInput =false;
+            }
+        }
+
+        return months;
+
     }
 
+    //Additional method: setting frequency of creation of centres (only use when MVP is complete!)
 
+    public static int getFrequencyOfCentresInput(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter how frequent new centres should be created in months:");
+        int frequency = scanner.nextInt();
 
+        while(frequency < 1){
+            System.out.println("Enter valid number:");
+            frequency = scanner.nextInt();
+        }
+
+        return frequency;
+    }
 
 
     /**
      * Test via reading back in the input, if there is additional time
      */
 
-    public static void displayResults(){
-
-
+    public static void main(String[] args) {
+        System.out.println("Months: " + getDurationInput());
     }
-
 
 
 
