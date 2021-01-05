f8c72a0e1f1b4d45b37725f6502bd86749c12ff5
MrFletcha <jwefletcher@hotmail.co.uk>
2021-01-05 11:42:48 +0000

    Hashmap for opencentres and boolean return method for if centre is full

--- src/main/java/com/sparta/spartaSimulator/controller/CentreManager.java
+++ src/main/java/com/sparta/spartaSimulator/controller/CentreManager.java
@@ -2,15 +2,31 @@ package com.sparta.spartaSimulator.controller;
 
 import com.sparta.spartaSimulator.model.TraineeCentre;
 
+import java.util.HashMap;
+
 public class CentreManager {
+    private HashMap<Integer, TraineeCentre> openCentres;
+
     // Capacity
     // Getters &
     // Parameters
     // Calls factory to have centre object creates
     // Maintain how many centre objects
-    // Create centres
-    public static TraineeCentre createCentre() {
-        TraineeCentre traineeCentre = new TraineeCentre();
-        return traineeCentre;
+    public static CreationInt creatingFactory()
+    {
+        return Factory.factory(2);
     }
+
+    public boolean isFull()
+    {
+        for(TraineeCentre centre: openCentres.values())
+        {
+            if(centre.getCentreStatus() == TraineeCentre.CentreStatus.FULL)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
