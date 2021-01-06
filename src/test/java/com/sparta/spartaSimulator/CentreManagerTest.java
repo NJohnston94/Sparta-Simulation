881ecd896a649f982a5083d1289c78d9a45cafd5
RashawnKH <rashawn1999@hotmail.com>
2021-01-06 16:33:46 +0000

    Add a test to check for waiting list priority

--- src/test/java/com/sparta/spartaSimulator/CentreManagerTest.java
+++ src/test/java/com/sparta/spartaSimulator/CentreManagerTest.java
@@ -3,7 +3,10 @@ package com.sparta.spartaSimulator;
 import com.sparta.spartaSimulator.controller.CentreManager;
 import com.sparta.spartaSimulator.controller.Centres;
 import com.sparta.spartaSimulator.controller.Factory;
+import com.sparta.spartaSimulator.controller.TraineeManager;
 import com.sparta.spartaSimulator.model.Trainee;
+import com.sparta.spartaSimulator.model.TraineeCentre;
+import com.sparta.spartaSimulator.model.WaitingList;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -38,5 +41,27 @@ public class CentreManagerTest {
         Assertions.assertEquals(1, CentreManager.getTrainees());
         Centres centre2 = CentreManager.createCentre(1);
         Assertions.assertEquals(1, CentreManager.getNumberOfFullCentres());
+    }
+
+
+    @Test
+    public void checkWaitingListPriority(){
+        Centres centres = CentreManager.createCentre();
+        Centres centres1 = CentreManager.createCentre();
+        centres.setCentreStatus(TraineeCentre.CentreStatus.FULL);
+
+        Trainee trainee = new Trainee();
+        Trainee trainee1 = new Trainee();
+
+        WaitingList.addTrainees(trainee);
+        WaitingList.addTrainees(trainee1);
+        final int size = WaitingList.getWaitingListSize();
+
+        TraineeManager.createTrainees();
+
+        CentreManager.addTrainees(CentreManager.openCentres);
+
+        Assertions.assertNotEquals(size, WaitingList.getWaitingListSize());
+        
     }
 }
