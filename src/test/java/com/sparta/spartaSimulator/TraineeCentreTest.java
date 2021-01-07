9b2ea9458227205e03d887326cb4ce97600d9a13
RashawnKH <rashawn1999@hotmail.com>
2021-01-07 12:31:35 +0000

    Tests for newly added setters and getters

--- src/test/java/com/sparta/spartaSimulator/TraineeCentreTest.java
+++ src/test/java/com/sparta/spartaSimulator/TraineeCentreTest.java
@@ -1,7 +1,9 @@
 package com.sparta.spartaSimulator;
 
 import com.sparta.spartaSimulator.model.BootCamp;
+import com.sparta.spartaSimulator.model.TechCentre;
 import com.sparta.spartaSimulator.model.TraineeCentre;
+import com.sparta.spartaSimulator.model.TrainingHub;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -18,4 +20,45 @@ public class TraineeCentreTest {
         BootCamp bootcamp = new BootCamp();
         Assertions.assertEquals(500, bootcamp.getMaxCapacity());
     }
+
+    @Test
+    public void checkCanSetAge(){
+        BootCamp bootCamp = new BootCamp();
+        bootCamp.setAge(5);
+
+        Assertions.assertEquals(5, bootCamp.getAge());
+
+        TrainingHub trainingHub = new TrainingHub();
+        trainingHub.setAge(2);
+
+        Assertions.assertEquals(2, trainingHub.getAge());
+
+        TechCentre techCentre = new TechCentre();
+        techCentre.setAge(7);
+
+        Assertions.assertEquals(7, techCentre.getAge());
+
+
+    }
+
+    @Test
+    public void checkSafePeriodIsSet(){
+
+        BootCamp bootCamp = new BootCamp();
+
+        Assertions.assertEquals(3, bootCamp.getSafePeriod());
+
+
+        TrainingHub trainingHub = new TrainingHub();
+
+        Assertions.assertEquals(2, trainingHub.getSafePeriod());
+
+
+        TechCentre techCentre = new TechCentre();
+
+        Assertions.assertEquals(2, techCentre.getSafePeriod());
+
+    }
+
+
 }
