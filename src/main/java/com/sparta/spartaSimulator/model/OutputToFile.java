87046096de25339a4eb2c7b62f0b42cc40853165
GSpratt97 <greg.spratt@icloud.com>
2021-01-07 16:38:20 +0000

    Option to clear or append data

--- src/main/java/com/sparta/spartaSimulator/model/OutputToFile.java
+++ src/main/java/com/sparta/spartaSimulator/model/OutputToFile.java
@@ -8,10 +8,20 @@ import java.io.IOException;
 public class OutputToFile {
     private static final String OUTPUT_PATH = "src/main/resources/Output.txt";
 
-
-    public static void writeDataToFile() {
+    public static void clearOutputFile() {
         try {
             FileWriter myWriter = new FileWriter(OUTPUT_PATH, false);
+            myWriter.write("");
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void appendDataToFile() {
+        try {
+            FileWriter myWriter = new FileWriter(OUTPUT_PATH, true);
             myWriter.write("End of simulation data:\n");
             myWriter.close();
         } catch (IOException e) {
